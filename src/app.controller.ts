93e8ed37dc13202c7a103edd57c469c84f48fb51
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2025-05-25 02:05:03 +0330

    Did all along the instructor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from "@nestjs/common"
-import { AppService } from "./app.service"
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common"
+import { ApiOperation } from "@nestjs/swagger"
 
-@Controller()
+@Controller("report/:type")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @ApiOperation({
+    summary: "Get all reports",
+  })
+  getAllReports() {
+    return []
+  }
+
+  @Get(":id")
+  @ApiOperation({
+    summary: "Get a report by ID",
+  })
+  getIncomeById() {
+    return {}
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: "Creates a report",
+  })
+  createReport() {
+    return "created"
+  }
+
+  @Put(":id")
+  @ApiOperation({
+    summary: "Updates a report by ID",
+  })
+  updateReportById() {
+    return "updated"
+  }
+
+  @Delete(":id")
+  @ApiOperation({
+    summary: "Deletes a report by ID",
+  })
+  deleteReportById() {
+    return "deleted"
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from "@nestjs/core"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import { apiReference } from "@scalar/nestjs-api-reference"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle("Expense App")
+    .setDescription("A simple project I did alongside the instructor to learn the NestJS's basics")
+    .setVersion("v1.0")
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("docs/swagger", app, documentFactory)
+
+  app.use(
+    "/docs",
+    apiReference({
+      theme: "deepSpace",
+      layout: "classic",
+      content: documentFactory,
+    }),
+  )
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
